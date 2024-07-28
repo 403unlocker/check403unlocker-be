8dcc2c30728d5a5166d2cad26d443cebdd25093b
Arshiya Khalili <106923820+ArshiyaDev@users.noreply.github.com>
2024-07-28 20:11:02 +0330

    imrpove structure of project

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
 from fastapi import FastAPI
-from services.Anti403 import GetAnti403Handler
-from services.Begzar import GetGetBegzarHandler
-from services.Vanillapp import GetVanillappHandler
-from services.Shecan import GetShecanHandler
-from fastapi.middleware.cors import CORSMiddleware
+from app.middleware.setup import setup_middlewares
+from app.routers.router_conf import router
+from app.app import fastapi_app
 
-app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  
-    allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
-)
+def setup_app() -> FastAPI:
+    fastapi_app.include_router(router)
+    setup_middlewares(fastapi_app)
+    return fastapi_app
 
-@app.get("/shecan")
-def GetShecan(url):
-    return GetShecanHandler(url)
 
-@app.get("/begzar")
-def GetBegzar(url):
-    return GetGetBegzarHandler(url)
-
-@app.get("/anti403")
-def Get403(url):
-    return GetAnti403Handler(url)
-
-@app.get("/vanillapp")
-def GetVanillapp(url):
-    return GetVanillappHandler(url)
+app: FastAPI = setup_app()
---
+++ run.py
@@ -0,0 +1,8 @@
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        reload=True,
+        port=8000,
+    )
